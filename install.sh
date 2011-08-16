01fd2dbf88b7f451a710786e1383e60c39d2ceed
Andrew Burgess <andrew8088@gmail.com>
2011-08-15 21:00:37 -0400

    added install.sh script to link dotfiles into place

---
+++ install.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+# install.sh
+# expects dotfiles repo to be kept in ~/bin/
+
+#alias zsh files
+ln -s ~/bin/dotfiles/zsh      ~/.zsh
+ln -s ~/bin/dotfiles/zsh/zshrc ~/.zshrc
+
+#alias vim files
+ln -s ~/bin/dotfiles/vim       ~/.vim
+ln -s ~/bin/dotfiles/vim/vimrc ~/.vimrc
+
+#alias git files
+ln -s ~/bin/dotfiles/gitconfig ~/.gitconfig
